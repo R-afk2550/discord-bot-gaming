dc011cb594f3d1b69c31edfec06c2713c1abb99b
R-afk2550 <kabreraedgar@gmail.com>
2026-02-07 16:59:07 -0800

    Updated cog_files in load_cogs method to include levels, economy, and logging.

--- bot.py
+++ bot.py
@@ -1,11 +1,29 @@
-cog_files = [
-    'roles',
-    'lfg',
-    'welcome',
-    'moderation',
-    'utility',
-    'events',
-    'levels',
-    'economy',
-    'logging'
-]
+import discord
+from discord.ext import commands
+
+class MyBot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix='!')
+
+    async def load_cogs(self):
+        cog_files = [
+            'roles',
+            'lfg',
+            'welcome',
+            'moderation',
+            'utility',
+            'events',
+            'levels',
+            'economy',
+            'logging'
+        ]
+        for cog in cog_files:
+            self.load_extension(f'cogs.{cog}')
+
+bot = MyBot()
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+bot.run('your_token_here')
